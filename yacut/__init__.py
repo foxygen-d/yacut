962de99cf2403f530c423e163f80f3c1d87a41b5
Anastasia Domnina <foxygen.d@yandex.ru>
2023-01-16 09:30:26 +0300

    ревью готов

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-BASE_URL = 'http://127.0.0.1:5000/'
+BASE_URL = 'http://localhost/'
 
 from . import api_views, error_handlers, models, views
